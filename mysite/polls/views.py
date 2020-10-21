b59d86b980cc30075a9b5937703e389aa1f14afb
Ramon Brignoli <ramon@Ramons-MacBook-Pro.local>
2020-10-21 14:31:13 -0300

    parte 5 tutorial

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,82 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Choice, Question
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("to no polls index.")
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    try:
+        selected_choice = question.choice_set.get(id=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'question': question,
+            'error_message': "you didnt select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
